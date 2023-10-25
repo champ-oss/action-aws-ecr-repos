904c242146a886af1f20bbbebbe9b323fa6e902f
Justin Reeves <reeves122@gmail.com>
2023-10-25 11:36:50 -0500

    add other imports

--- terraform_import.py
+++ terraform_import.py
@@ -1,5 +1,7 @@
-import sys
 import os
+import sys
 
 for name in sys.argv[1].splitlines():
     os.system(f"terraform import 'module.this[\"{name}\"].aws_ecr_repository.this' {name}")
+    os.system(f"terraform import 'module.this[\"{name}\"].aws_ecr_lifecycle_policy.this[0]' {name}")
+    os.system(f"terraform import 'module.this[\"{name}\"].aws_ecr_repository_policy.ecr_policy[0]' {name}")

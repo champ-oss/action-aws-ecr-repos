b9f33ad8b276339c28dc77c515d5df496bc6689e
Justin Reeves <reeves122@gmail.com>
2023-10-25 09:42:30 -0500

    test terraform-aws-ecr

--- main.tf
+++ main.tf
@@ -1,13 +1,11 @@
-#module "this" {
-#  for_each = toset([
-#    "foo"
-#  ])
-#  source               = "github.com/champ-oss/terraform-aws-ecr.git?ref=v1.0.88-7582d14"
-#  name                 = each.value
-#  trusted_accounts     = var.trusted_accounts
-#  image_tag_mutability = var.image_tag_mutability
-#  encryption_type      = var.encryption_type
-#  scan_on_push         = var.scan_on_push
-#  force_delete         = var.force_delete
-#  image_limit          = var.image_limit
-#}
+module "this" {
+  for_each             = toset(split("\n", trimspace(var.names)))
+  source               = "github.com/champ-oss/terraform-aws-ecr.git?ref=v1.0.88-7582d14"
+  name                 = each.value
+  trusted_accounts     = split("\n", trimspace(var.trusted_accounts))
+  image_tag_mutability = var.image_tag_mutability
+  encryption_type      = var.encryption_type
+  scan_on_push         = tobool(var.scan_on_push)
+  force_delete         = tobool(var.force_delete)
+  image_limit          = tonumber(var.image_limit)
+}

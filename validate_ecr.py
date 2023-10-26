50d0245c4e84865a720b299e22f4c7d9d1dd7fcf
Justin Reeves <reeves122@gmail.com>
2023-10-26 14:31:16 -0500

    create validate_ecr.py

---
+++ validate_ecr.py
@@ -0,0 +1,16 @@
+import json
+import sys
+
+import boto3
+
+name = sys.argv[1]
+expected_accounts = sys.argv[2].split(',')
+
+client = boto3.client('ecr', region_name='us-east-2')
+response = client.get_repository_policy(repositoryName=name)
+
+policy = json.loads(response.get('policyText'))
+aws_principals = policy['Statement'][0]['Principal']['AWS']
+
+for expected_account in expected_accounts:
+    assert expected_account in aws_principals, f'{aws_principals} does not contain {expected_account}'

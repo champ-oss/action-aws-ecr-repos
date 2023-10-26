71a83980ae97cc9492667dc4ccdadc68984edcb4
Justin Reeves <reeves122@gmail.com>
2023-10-26 15:03:40 -0500

    update assertion

--- example_ssm/main.tf
+++ example_ssm/main.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "accounts" {
   name  = "example-aws-accounts"
   type  = "StringList"
-  value = join(",", ["912455136424", "266458841044", "432418664414"])
+  value = join(",", ["266458841044", "432418664414"])
 }

ede4c7e008dd0df045ff0ec985df02aacf8d5608
Justin Reeves <reeves122@gmail.com>
2023-10-26 15:11:31 -0500

    add import

--- validate_ecr.py
+++ validate_ecr.py
@@ -3,14 +3,16 @@ import sys
 
 import boto3
 
-name = sys.argv[1]
+names = sys.argv[1].split(',')
 expected_accounts = sys.argv[2].split(',')
 
 client = boto3.client('ecr', region_name='us-east-2')
-response = client.get_repository_policy(repositoryName=name)
 
-policy = json.loads(response.get('policyText'))
-aws_principals = policy['Statement'][0]['Principal']['AWS']
+for name in names:
+    response = client.get_repository_policy(repositoryName=name)
 
-for expected_account in expected_accounts:
-    assert f'arn:aws:iam::{expected_account}:root' in aws_principals, f'{aws_principals} does not contain account: {expected_account}'
+    policy = json.loads(response.get('policyText'))
+    aws_principals = policy['Statement'][0]['Principal']['AWS']
+
+    for expected_account in expected_accounts:
+        assert f'arn:aws:iam::{expected_account}:root' in aws_principals, f'{aws_principals} does not contain account: {expected_account}'

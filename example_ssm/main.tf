3970229fcc256a2fffde3227deab0aea0b35490d
Justin Reeves <reeves122@gmail.com>
2023-10-26 12:29:04 -0500

    test trusted_accounts_ssm

--- example_ssm/main.tf
+++ example_ssm/main.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "accounts" {
   name  = "example-aws-accounts"
   type  = "StringList"
-  value = join(",", ["912455136424", "266458841044"])
+  value = join(",", ["912455136424", "266458841044", "432418664414"])
 }

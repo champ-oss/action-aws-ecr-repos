0ea0d54b333ceb55723023d284118a61f5482574
Justin Reeves <reeves122@gmail.com>
2023-10-26 12:24:38 -0500

    test trusted_accounts_ssm

--- main.tf
+++ main.tf
@@ -1,27 +1,21 @@
 data "aws_caller_identity" "this" {}
 
 locals {
+  local_account_list        = [data.aws_caller_identity.this.account_id]
   trusted_accounts_list     = split("\n", trimspace(var.trusted_accounts))
-  trusted_accounts_ssm_list = var.trusted_accounts_ssm != "" ? split(",", data.aws_ssm_parameter.this[0].value) : []
+  trusted_accounts_ssm_list = var.trusted_accounts_ssm != "" ? split(",", data.aws_ssm_parameter.trusted_accounts_ssm[0].value) : []
 }
 
-data "aws_ssm_parameter" "this" {
+data "aws_ssm_parameter" "trusted_accounts_ssm" {
   count = var.trusted_accounts_ssm != "" ? 1 : 0
   name  = var.trusted_accounts_ssm
 }
 
-output "test" {
-  value     = local.trusted_accounts_ssm_list
-  sensitive = false
-}
-
 module "this" {
-  for_each = toset(split("\n", trimspace(var.names)))
-  source   = "github.com/champ-oss/terraform-aws-ecr.git?ref=v1.0.88-7582d14"
-  name     = each.value
-  trusted_accounts = concat([
-    data.aws_caller_identity.this.account_id
-  ], local.trusted_accounts_list, local.trusted_accounts_ssm_list)
+  for_each             = toset(split("\n", trimspace(var.names)))
+  source               = "github.com/champ-oss/terraform-aws-ecr.git?ref=v1.0.88-7582d14"
+  name                 = each.value
+  trusted_accounts     = toset(concat(local.local_account_list, local.trusted_accounts_list, local.trusted_accounts_ssm_list))
   image_tag_mutability = var.image_tag_mutability
   encryption_type      = var.encryption_type
   scan_on_push         = tobool(var.scan_on_push)
